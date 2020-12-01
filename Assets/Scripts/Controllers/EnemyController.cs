a4ec8849504a48e3cfef0f60524a6b9f0ddd4ff9
Dang Pham <dpham1@uno.edu>
2020-12-01 16:54:59 -0600

    Enemy AI: chasing the player if the player gets close enough

---
+++ Assets/Scripts/Controllers/EnemyController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyController : MonoBehaviour
+{
+    public float lookRange = 10f;
+
+    Transform target;
+    NavMeshAgent thisAgent;
+    bool isChasing = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        thisAgent = GetComponent<NavMeshAgent>();
+        target = PlayerManager.instance.player.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = Vector3.Distance(target.position, transform.position);
+
+        if(distance <= lookRange)
+        {
+            thisAgent.SetDestination(target.position);
+            isChasing = true;
+
+            if(distance <= thisAgent.stoppingDistance)
+            {
+                FaceTarget();
+            }
+        }
+
+        if(isChasing && distance > lookRange)
+        {
+            isChasing = false;
+            thisAgent.SetDestination(transform.position);
+        }
+
+    }
+
+    void FaceTarget()
+    {
+        Vector3 direction = (target.position - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, lookRange);
+    }
+}
