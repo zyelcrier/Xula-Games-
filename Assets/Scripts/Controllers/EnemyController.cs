bcce84ee9ad19b62da47dd1451be4da7da27f628
Dang Pham <dpham1@uno.edu>
2020-12-17 15:28:49 -0600

    Enemy behaviors: wandering and attacking

--- Assets/Scripts/Controllers/EnemyController.cs
+++ Assets/Scripts/Controllers/EnemyController.cs
@@ -6,6 +6,11 @@ using UnityEngine.AI;
 public class EnemyController : MonoBehaviour
 {
     public float lookRange = 10f;
+    public float stopDistance = 2f;
+    public List<Transform> wanderingPoints = new List<Transform>();
+    Transform currentWanderDestination;
+    float timeBetweenAttacks = 2f;
+    float lastAttackTime = 0f;
 
     Transform target;
     NavMeshAgent thisAgent;
@@ -16,6 +21,9 @@ public class EnemyController : MonoBehaviour
     {
         thisAgent = GetComponent<NavMeshAgent>();
         target = PlayerManager.instance.player.transform;
+        currentWanderDestination = wanderingPoints[Random.Range(0, wanderingPoints.Count)];
+        wanderingPoints.Remove(currentWanderDestination);
+        thisAgent.SetDestination(currentWanderDestination.position);
     }
 
     // Update is called once per frame
@@ -28,9 +36,22 @@ public class EnemyController : MonoBehaviour
             thisAgent.SetDestination(target.position);
             isChasing = true;
 
-            if(distance <= thisAgent.stoppingDistance)
+            if(distance <= stopDistance)
             {
+                thisAgent.ResetPath();
                 FaceTarget();
+                
+                if(lastAttackTime == 0f)
+                {
+                    target.GetComponent<PlayerStat>().takeDamage(GetComponent<EnemyStat>().damage);
+                    lastAttackTime = Time.time;
+                }
+
+                if(Time.time - lastAttackTime > timeBetweenAttacks)
+                {
+                    lastAttackTime = Time.time;
+                    target.GetComponent<PlayerStat>().takeDamage(GetComponent<EnemyStat>().damage);
+                }
             }
         }
 
@@ -40,6 +61,10 @@ public class EnemyController : MonoBehaviour
             thisAgent.SetDestination(transform.position);
         }
 
+        if(!isChasing)
+        {
+            wander();
+        }
     }
 
     void FaceTarget()
@@ -53,5 +78,34 @@ public class EnemyController : MonoBehaviour
     {
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(transform.position, lookRange);
+    }
+
+    void wander()
+    {
+        float distance = Vector3.Distance(currentWanderDestination.position, transform.position);
+
+        if(thisAgent.destination != currentWanderDestination.position)
+        {
+            thisAgent.SetDestination(currentWanderDestination.position);
+        }
+
+        if (distance <= 3)
+        {
+            GetComponent<NavMeshAgent>().isStopped = true;
+            thisAgent.ResetPath();
+            currentWanderDestination = getNextWanderPosition();
+            thisAgent.SetDestination(currentWanderDestination.position);
+        }
+    }
+
+    Transform getNextWanderPosition()
+    {
+        Transform nextPosition;
+        
+        nextPosition = wanderingPoints[Random.Range(0, wanderingPoints.Count)];
+        wanderingPoints.Remove(nextPosition);
+        wanderingPoints.Add(currentWanderDestination);
+
+        return nextPosition;
     }
 }
